d033ee9a3d2840507c39b05df786c47c53af77b2
Amie Wilt <amie.wilt@gmail.com>
2019-07-29 11:33:13 -0400

    Update redux-offline and NetInfo

--- packages/aws-appsync-react/src/rehydrated-rn.tsx
+++ packages/aws-appsync-react/src/rehydrated-rn.tsx
@@ -3,81 +3,84 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 import * as React from "react";
-import { View, Text, StyleSheet, NetInfo, ViewPropTypes } from "react-native";
-import * as PropTypes from 'prop-types';
+import { View, Text, StyleSheet, ViewPropTypes } from "react-native";
+import NetInfo from "@react-native-community/netinfo";
+import * as PropTypes from "prop-types";
 
-import AWSAppSyncClient from 'aws-appsync';
-import { RehydratedState } from './index'
+import AWSAppSyncClient from "aws-appsync";
+import { RehydratedState } from "./index";
 
 export interface RehydrateProps {
-    rehydrated: boolean;
-    children: React.ReactNode;
-    style: any;
+  rehydrated: boolean;
+  children: React.ReactNode;
+  style: any;
 }
 
 const Rehydrate = (props: RehydrateProps) => (
-    <View style={[styles.container, props.style || {}]} >
-        {props.rehydrated ? props.children : <Text>Loading...</Text>}
-    </View>
+  <View style={[styles.container, props.style || {}]}>
+    {props.rehydrated ? props.children : <Text>Loading...</Text>}
+  </View>
 );
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-    },
+  container: {
+    flex: 1
+  }
 });
 
 export interface RehydratedProps {
-    render?: ((props: { rehydrated: boolean }) => React.ReactNode);
-    children?: React.ReactNode;
-    loading?: React.ComponentType<any>;
-    style?: any;
+  render?: (props: { rehydrated: boolean }) => React.ReactNode;
+  children?: React.ReactNode;
+  loading?: React.ComponentType<any>;
+  style?: any;
 }
 
-export default class Rehydrated extends React.Component<RehydratedProps, RehydratedState> {
+export default class Rehydrated extends React.Component<
+  RehydratedProps,
+  RehydratedState
+> {
+  static contextTypes = {
+    client: PropTypes.instanceOf(AWSAppSyncClient).isRequired
+  };
 
-    static contextTypes = {
-        client: PropTypes.instanceOf(AWSAppSyncClient).isRequired,
+  static propTypes = {
+    render: PropTypes.func,
+    children: PropTypes.node,
+    loading: PropTypes.node,
+    style: ViewPropTypes ? ViewPropTypes.style : View.propTypes.style
+  };
+
+  constructor(props, context) {
+    super(props, context);
+
+    this.state = {
+      rehydrated: false
     };
+  }
 
-    static propTypes = {
-        render: PropTypes.func,
-        children: PropTypes.node,
-        loading: PropTypes.node,
-        style: ViewPropTypes ? ViewPropTypes.style : View.propTypes.style,
-    };
+  async componentWillMount() {
+    await this.context.client.hydrated();
+    await NetInfo.isConnected.fetch();
 
-    constructor(props, context) {
-        super(props, context);
+    this.setState({
+      rehydrated: true
+    });
+  }
 
-        this.state = {
-            rehydrated: false
-        };
+  render() {
+    const { render, children, loading, style } = this.props;
+    const { rehydrated } = this.state;
+
+    if (render) return render({ rehydrated });
+
+    if (children) {
+      if (loading) return rehydrated ? children : loading;
+
+      return (
+        <Rehydrate rehydrated={rehydrated} style={style}>
+          {children}
+        </Rehydrate>
+      );
     }
-
-    async componentWillMount() {
-        await this.context.client.hydrated();
-        await NetInfo.isConnected.fetch();
-
-        this.setState({
-            rehydrated: true
-        });
-    }
-
-    render() {
-        const { render, children, loading, style } = this.props;
-        const { rehydrated } = this.state;
-
-        if (render) return render({ rehydrated });
-
-        if (children) {
-            if (loading) return rehydrated ? children : loading;
-
-            return (
-                <Rehydrate rehydrated={rehydrated} style={style} >
-                    {children}
-                </Rehydrate>
-            );
-        }
-    }
+  }
 }
